c8879387fc84a2e45b2f92143542183b6e58a342
PabloLeyes <pablo_leyes_25@hotmail.com>
2018-07-09 11:03:08 -0300

    Update crud_data.rb
    Update

--- lib/crud_data.rb
+++ lib/crud_data.rb
@@ -11,7 +11,7 @@ class CRUDData
   end
 
   # TODO: ver como que vou fazer pra checar
-  # SELECT * FROM USER_SYS_PRIVS; checa privilegios de usuario.
+  # SELECT * FROM DBA_TAB_PRIVS; checa privilegios de usuario.
   # SELECT * FROM DBA_ROLE_PRIVS; checa privilegios de perfil.
   def check_permission(user_type)
     user_type
